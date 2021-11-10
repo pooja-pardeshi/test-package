c7c7fe286e4ca7a4aced5f7de1d044f64cff24ec
Pooja <pooja.pardeshi@borngroup.com>
2021-11-10 20:26:01 +0530

    changes

--- registration.php
+++ registration.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * Test Package
+ */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'PoojaPardeshi_TestPackage',
